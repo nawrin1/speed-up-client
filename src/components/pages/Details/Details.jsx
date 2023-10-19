71796d25607c57990e377d3a732a9c88f3cd607d
Rakibul88 <rakibulhasan999000@gmail.com>
2023-10-20 00:16:08 +0600

    details page is completed

--- src/components/pages/Details/Details.jsx
+++ src/components/pages/Details/Details.jsx
@@ -22,20 +22,20 @@ const Details = () => {
     // console.log(cars)
 
     return (
-        <div className="max-w-6xl mx-auto p-4">
+        <div className="max-w-6xl mx-auto p-4 mb-10">
             <h2 className="text-7xl font-bold text-center font-Oxanium mt-10 mb-9 ">{brandName}</h2>
             <div>
-                <div className="flex justify-center relative top-8 mb-3  ">
-                    <img className="w-[75%] rounded-3xl"src={image} alt="" />
+                <div className="flex justify-center relative top-10 mb-3  ">
+                    <img className="w-[65%] shadow-xl shadow-slate-600 rounded-3xl"src={image} alt="" />
 
                 </div>
                 <div>
-                    <div className="border-2 border-slate-700 rounded-3xl w-[40%] p-4 bg-slate-300 mb-6" >
-                    <h2 className="text-4xl font-semibold text-justify font-Oxanium mb-3 mt-5">Name:{name}</h2>
-                    <h2 className="text-4xl font-semibold text-justify font-Oxanium mb-10">Type:{type}</h2>
+                    <div className="border-2 border-slate-700 rounded-3xl w-[40%] lg:p-4 md:p-4 p-2 bg-slate-300 mb-6 hover:shadow-2xl hover:shadow-slate-800" >
+                    <h2 className="lg:text-4xl md:text-4xl text-xl font-semibold text-justify font-Oxanium mb-3 mt-5">Name: {name}</h2>
+                    <h2 className="lg:text-4xl md:text-4xl text-xl font-semibold text-justify font-Oxanium mb-6">Type: {type}</h2>
                     </div>
 
-                    <p className="text-xl font-medium text-justify font-Oxanium">{description}</p>
+                    <p className="text-xl font-medium text-justify font-Oxanium mt-12">{description}</p>
 
                 </div>
             </div>
